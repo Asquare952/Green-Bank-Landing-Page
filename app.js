b019dac09d850806e089d7b15782a6f496bd7877
Asquare952 <abdulafeezabdulazeez894@gmail.com>
2024-06-30 18:50:11 +0100

    add completed ui

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const sideBar = document.querySelector(".side-bar-main");
+const answer = document.getElementById("answer");
+const showanswer = document.getElementById("showAnswer");
 // const closeSideBar = 
 function showSideBar() {
   sideBar.classList = ("active");
+}
+
+function showAnswer() {
+  if () {
+    
+  } else {
+    
+  }
 }
